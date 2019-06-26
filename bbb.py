73f6c10dc1294d1ea1ea5d50adb3fb132df3d66f
loveyouforever04 <52123323+loveyouforever04@users.noreply.github.com>
2019-06-26 19:53:47 +0800

    mmmm.py

--- bbb.py
+++ bbb.py
@@ -20,13 +20,3 @@ print(x)
 print( "随机数 0-1 是：" )
 x = random.random()
 print(x)
-# 产生  a 到 b 之间的随机浮点数，区间可以不是整数
-a = 3.3
-b = 10.7
-c = random.uniform(a,b)
-print("在 %f - %f 之间的随机数是：%f" %(a, b, c) )
-
-# 从序列中随机选取一个元素
-d = "tomorrow"
-e = random.choice(d)
-print("在字符串 %s 中随机挑选的字符是：%s" %(d, e)  )
---
+++ xq
@@ -0,0 +1,11 @@
+*- coding: UTF-8 -*-
+
+# Filename : 02-switching.py
+# author by : （学员ID)
+
+# 目的:
+# 掌握基本的赋值，输入及输出方法
+# 掌握 print 代入模式
+
+# 用户输入
+x = input('输入 x 值: ')
